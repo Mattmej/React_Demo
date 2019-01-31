40868804a77be4dc0938ea73d69340f18b1f6b43
unknown <mattcmej@gmail.com>
2019-01-30 18:12:51 -0800

    Finish Section 3 Lesson 46 of Udemy course. Add css file to 'Person' directory.

--- src/App.js
+++ src/App.js
@@ -28,6 +28,20 @@ class App extends Component {
     })
   }
 
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: "Max", age: 28 },
+
+        /* The input here will be the target into which we typed.*/
+        { name: event.target.value, age: 29 },
+
+        { name: "Stephanie", age: 26 }
+      ]
+    
+    })
+  }
+
 
   render() {
 
@@ -36,8 +50,23 @@ class App extends Component {
         <h1>Hi I am a React app!</h1>
         <p>This is really working!</p>
 
-        {/* Execute dynamic code */}
-        <button onClick={this.switchNameHandler.bind(this, "Maximilian")}>Switch Name</button>
+        {/* Arrow function that takes no arguments,
+              but theoretically it would receive an event object
+              (we won't use it here)
+
+            Returns a function call (note the parentheses).
+            It is not getting executed immediately.
+            Instead, an anonymous function is passed,
+              which will be executed on a click
+              and returns the result of the function being executed.
+
+        
+            Implicitly adds a return keyword in front of the code 
+              that comes directly after the arrow, if it is written in one line.
+              (This is a feature of arrow functions)
+        */}
+
+        <button onClick={() => this.switchNameHandler("Maximilian!!")}>Switch Name</button>
 
         <Person 
           name={this.state.persons[0].name} 
@@ -46,6 +75,7 @@ class App extends Component {
           name={this.state.persons[1].name} 
           age={this.state.persons[1].age}
           click={this.switchNameHandler.bind(this, "Max!")}
+          changed={this.nameChangedHandler}
           >
             Hobbies: Racing
         </Person>

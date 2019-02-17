63e3ec3f4c553f149c4ab024400e7046eb8862d9
unknown <mattcmej@gmail.com>
2019-02-16 16:19:51 -0800

    Convert Person.js and Persons.js into class components. Finish Section 7 Lecture 87 of Udemy course.

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,24 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 // import './Person.css';
 import classes from './Person.css';
 
-const person = (props) => { 
+class Person extends Component { 
     // return <p>I'm a Person and I am {Math.floor(Math.random() * 30)} years old!</p>
 
-    console.log('[Person.js] rendering...');
+    render() {
+        console.log('[Person.js] rendering...');
 
-    return (
-        
-        // NOTE: Here, the CSS class is unique.
-        <div className={classes.Person}>
-            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
-            <p>{props.children}</p>
+        return (
+            
+            // NOTE: Here, the CSS class is unique.
+            <div className={classes.Person}>
+                <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
+                <p>{this.props.children}</p>
+    
+                {/* Here, we both listen to changes and pass down the state to the "Person" object.
+                    We then output the name as the value of the input */}
+                <input type="text" onChange={this.props.changed} value={this.props.name}/>
+            </div>
+        );
+    };
 
-            {/* Here, we both listen to changes and pass down the state to the "Person" object.
-                We then output the name as the value of the input */}
-            <input type="text" onChange={props.changed} value={props.name}/>
-        </div>
-    )
+    
 };
 
-export default person;
+export default Person;
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,18 +1,45 @@
 // a functional component
 // because I don't plan on working with state here.
 
-import React from 'react';
+import React, {Component} from 'react';
 import Person from './Person/Person';
 
-const persons = (props) => {
+class Persons extends Component {
 
+  static getDerivedStateFromProps(props, state) {
+    console.log('[Persons.js] getDerivedStateFromProps');
+    return state;
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    // must return true or false.
+    // true if react should continue updating,
+    // false if it shouldn't.
+
+    console.log('[Persons.js] shouldComponentUpdate');
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('[Persons.js] getSnapshotBeforeUpdate');
+    return { message: 'Snapshot!' };
+  }
+
+  // This will trigger when we change the text in the input.
+  // Will take getSnapshotBeforeUpdate's snapshot
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('[Persons.js] componentDidUpdate');
+    console.log(snapshot);
+  }
+
+  render() {
     console.log('[Persons.js] rendering...');
 
-    return props.persons.map( (person, index) => {
+    return this.props.persons.map( (person, index) => {
         // return what you want to map this item into.
         // Handlers are removed for now.
         return (<Person 
-          click={() => props.clicked(index)}
+          click={() => this.props.clicked(index)}
           name={person.name} 
           age={person.age} 
           // NOT a good key. If the list changes, every element 
@@ -20,13 +47,15 @@ const persons = (props) => {
           // key={index}
 
           key={person.id}
-          changed={(event) => props.changed(event, person.id)} />
+          changed={(event) => this.props.changed(event, person.id)} />
         );
     });
+  }
 
+    
 };
 
 
 
 
-export default persons;
+export default Persons;

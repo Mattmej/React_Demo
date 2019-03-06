c2d85885f96213323e0bd0c7aa9a2ce6347b7c5c
unknown <mattcmej@gmail.com>
2019-03-05 21:54:54 -0800

    On Section 7 Lesson 97.

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 // import './Person.css';
+
+import Aux from '../../../hoc/Auxiliary';
 import classes from './Person.css';
 
 class Person extends Component { 
@@ -8,17 +10,31 @@ class Person extends Component {
     render() {
         console.log('[Person.js] rendering...');
 
+
+        // Now this returns an array of JSX objects.
+        // NOTE: each element must have a unique key.
+        // Here, we are not randomly generating keys.
+        // For now, we are assigning our own keys to each object.
         return (
+
+            <Aux>
+               
             
-            // NOTE: Here, the CSS class is unique.
-            <div className={classes.Person}>
-                <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
-                <p>{this.props.children}</p>
-    
-                {/* Here, we both listen to changes and pass down the state to the "Person" object.
-                    We then output the name as the value of the input */}
-                <input type="text" onChange={this.props.changed} value={this.props.name}/>
-            </div>
+                <p key="i1" onClick={this.props.click}>
+                    I'm {this.props.name} and I am {this.props.age} years old!
+                </p>
+                <p key="i2">{this.props.children}</p>
+
+                 {/* Here, we both listen to changes and pass down the state to the "Person" object.
+                //     We then output the name as the value of the input */}
+                <input 
+                    key="i3"
+                    type="text" 
+                    onChange={this.props.changed} 
+                    value={this.props.name}
+                />
+            </Aux>  
+        
         );
     };
 
---
+++ src/hoc/Auxiliary.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+// "children" is a special property
+// It ouputs whatever is entered between 
+// the opening and closing tag of this component.
+const aux = props => props.children;
+
+export default aux;
